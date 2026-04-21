263c095fa37503a83a81f7f226efb43a494156d7
unknown <nguyenphon1003@gmail.com>
2026-04-21 18:06:52 +0700

    setup auth-service using spring cloud config

--- auth-service/src/main/java/com/trainng/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/trainng/auth_service/AuthServiceApplication.java
@@ -2,12 +2,20 @@ package com.trainng.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/auth")
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello from Auth Service!";
 	}
 
 }
